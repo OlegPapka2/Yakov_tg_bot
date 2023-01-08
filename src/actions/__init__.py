ce26a7c12439ddb507db543ac3e5adcf853fe706
Oleh Papka <olegpapka2@gmail.com>
2023-01-08 21:38:33 +0200

    Change actions execution time

--- src/actions/__init__.py
+++ src/actions/__init__.py
@@ -1,4 +1,6 @@
-from datetime import timedelta
+from datetime import timedelta, time
+
+import pytz
 
 from .ru_losses import ru_losses
 from .crypto import crypto
@@ -8,7 +10,9 @@ from .weather import weather
 
 def register_actions(updater):
     jq = updater.job_queue
-    jq.run_repeating(ru_losses, interval=timedelta(days=1), first=1)
+    timezone = pytz.timezone('Europe/Kyiv')
+
     jq.run_repeating(crypto, interval=timedelta(days=1), first=1)
     jq.run_repeating(currency, interval=timedelta(days=1), first=1)
-    jq.run_repeating(weather, interval=timedelta(days=1), first=1)
+    jq.run_daily(weather, time=time(hour=7, minute=30, second=0, tzinfo=timezone))
+    jq.run_daily(ru_losses, time=time(hour=10, minute=0, second=0, tzinfo=timezone))
